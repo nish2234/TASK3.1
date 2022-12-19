ce3fa7a70e59d745e7877fb7111fb5ca7d9d4267
nish2234 <nishant2003official@gmail.com>
2022-12-19 13:06:20 +0530

    Initial commit for loginpage

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import backg from './backg.jpg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [mail , setmail] = useState("");
+  const [pass , setpass] = useState("");
+  const setemail = (event) => {
+    setmail(event.target.value);
+        
+  }
+  const setpassword = (event) => {
+    setpass(event.target.value);
+    
+  }
+  
+
+return(
+ <>
+ <div className="main">
+    
+    <div className="box1">
+        
+ 
+      <div className="form">
+        <p className="t">LOGIN / SIGNUP</p>
+      <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" onChange={setemail} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" onChange={setpassword} class="form-control" id="exampleInputPassword1"/>
+  </div>
+  
+  <div className="twobut">
+  <button type="submit" class="btn btn-primary">Login</button>
+  <button type="submit" class="btn btn-primary">SignUp</button>
+  </div>
+</form>
+
+
+     
+      </div>
+        
+        
+    </div>
+</div>
+ </>
+);
+}
+
+export default App;
